0589a0badb31e1e0a812d6fd0c6507ab956bea65
Franz <franzbascope123@gmail.com>
2019-04-22 01:08:43 -0400

    añadir bloqueo por deudas

--- assets/js/login.js
+++ assets/js/login.js
@@ -19,7 +19,7 @@ $(document).ready(function($) {
                     'Content-Type': 'application/json'
                 },
                 'type': 'POST',
-                'url': "http://sisnur.nur.edu:8085/api/Registros/Login",
+                'url': "http://wsnotas.nur.edu:8880/api/Registros/Login",
                 'data': JSON.stringify(usuario),
                 'dataType': 'json',
                 'success': resultado,
@@ -30,16 +30,31 @@ $(document).ready(function($) {
     });
 
     function resultado(resultado) {
-        if (resultado.includes("Bloqueo") || resultado.includes("error") || resultado == '') {
-            swal("Bloqueo", "Tiene deuda pendiente.", "error")
-        } else {
             localStorage.setItem("token", resultado);
+            debugger
+            obtenerBloqueo(resultado)
             localStorage.setItem("usrLog", JSON.stringify(resultado.Message));
             var url = 'adminSite/dashboard.html'
             $(location).attr("href", url);
-        }
     }
-
+    function obtenerBloqueo(token)
+        {
+            $.ajax({
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                },
+                'type': 'POST',
+                 async: false,
+                'url': "http://sisnur.nur.edu:8085/api/Registros/GetAlumnoBloqueo",
+                'dataType': 'json',
+            }).done(function(response) {
+                debugger
+                var data = response.Data.toLowerCase();
+                localStorage.setItem("tieneBloqueo", data.includes('bloqueo') ?  1 :0);
+            });
+        }
     function errorLogin() {
         swal("Error", "Usuario o Pin incorrectos.", "error")
     }
